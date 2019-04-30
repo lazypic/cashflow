fb90ecc382a6676fce90afb72e1bd8b201911b3b
woong <woong@lazypic.org>
2019-04-30 18:05:40 +0900

    짧은 분기문자로 변경하는 코드를 추가함.

--- timefunc.go
+++ timefunc.go
@@ -11,7 +11,7 @@ var RFC3339 = regexp.MustCompile(`^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[+-]\d{2}:
 var Quarter = regexp.MustCompile(`^\d{4}Q\d{1}$`)
 var ShortQuarter = regexp.MustCompile(`^\d{1}Q\d{2}$`)
 
-// RFC3339 시간형식을 분기로 변환한다.₩
+// RFC3339 시간형식을 분기로 변환한다.
 func RFC3339_to_Quarter(rfctime string) (string, error) {
 	if !RFC3339.MatchString(rfctime) {
 		return rfctime, errors.New("time string is not RFC3339 format.")
@@ -21,4 +21,16 @@ func RFC3339_to_Quarter(rfctime string) (string, error) {
 		return rfctime, err
 	}
 	return fmt.Sprintf("%dQ%d", t.Year(), t.Month()/3+1), nil
+}
+
+// RFC3339 시간형식을 짧은 분기문자로 변환한다.
+func RFC3339_to_ShortQuarter(rfctime string) (string, error) {
+	if !RFC3339.MatchString(rfctime) {
+		return rfctime, errors.New("time string is not RFC3339 format.")
+	}
+	t, err := time.Parse(time.RFC3339, rfctime)
+	if err != nil {
+		return rfctime, err
+	}
+	return fmt.Sprintf("%dQ%s", t.Month()/3+1, rfttime[2:4]), nil
 }
