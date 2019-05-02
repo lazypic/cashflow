e336c2fae37a4fa067be754baa838649136887ab
woong <woong@lazypic.org>
2019-05-02 17:06:04 +0900

    분기 하나가 출력되도록 선언함.

--- cashflow.go
+++ cashflow.go
@@ -10,16 +10,17 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 	"github.com/ladydascalie/currency"
 )
 
 func main() {
 	// 인수처리부
-	now := time.Now().Format(time.RFC3339)
+	now := time.Now()
 	regionPtr := flag.String("region", "ap-northeast-2", "aws region name")
 	profilePtr := flag.String("profile", "lazypic", "aws credentials profile name")
 	tablePtr := flag.String("table", "cashflow_demo", "aws dynamodb table name")
-	datePtr := flag.String("date", now, "deposit date")
+	datePtr := flag.String("date", now.Format(time.RFC3339), "deposit date")
 	amountPtr := flag.Int64("amount", 0, "deposit amount (Required)")
 	recipientPtr := flag.String("recipient", "lazypic", "recipient")
 	projectPtr := flag.String("project", "none", "project name")
@@ -27,7 +28,7 @@ func main() {
 	unitPtr := flag.String("unit", "KRW", "mometary unit")
 	senderPtr := flag.String("sender", "", "sender (Required)")
 	typePtr := flag.String("type", "donation", "type name: donation, investment, profit(일시수익), contract(계약금), interim(중도금), balance(잔금), addon(추가금)")
-	actualDatePtr := flag.String("actualdate", now, "actual deposit date")
+	actualDatePtr := flag.String("actualdate", now.Format(time.RFC3339), "actual deposit date")
 	actualAmountPtr := flag.Int64("actualamount", 0, "actual deposit amount")
 	helpPtr := flag.Bool("help", false, "print help")
 	flag.Parse()
@@ -47,12 +48,6 @@ func main() {
 		flag.PrintDefaults()
 		os.Exit(0)
 	}
-	if *senderPtr == "" || *amountPtr == 0 {
-		// 분기별 데이터 가지고 오기
-		// 분기별 보고 출력하기.
-		fmt.Println("drawing chart")
-		os.Exit(0)
-	}
 
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
@@ -60,6 +55,53 @@ func main() {
 		Profile:           *profilePtr,
 	}))
 	db := dynamodb.New(sess)
+
+	if *senderPtr == "" || *amountPtr == 0 {
+		// 분기별 데이터 가지고 와서 출력하기
+		filt := expression.Name("Quarter").Equal(expression.Value("2019Q2"))
+		proj := expression.NamesList(expression.Name("DepositAmount"))
+		expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+		params := &dynamodb.ScanInput{
+			ExpressionAttributeNames:  expr.Names(),
+			ExpressionAttributeValues: expr.Values(),
+			FilterExpression:          expr.Filter(),
+			ProjectionExpression:      expr.Projection(),
+			TableName:                 aws.String(*tablePtr),
+		}
+		result, err := db.Scan(params)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+
+		var amount int64
+		for _, i := range result.Items {
+			item := Item{}
+
+			err = dynamodbattribute.UnmarshalMap(i, &item)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+			amount += item.DepositAmount
+		}
+		fmt.Println("2Q19")
+		fmt.Println(amount)
+
+		for y := now.Year() - 1; y <= now.Year()+2; y++ {
+			for q := 1; q <= 4; q++ {
+				partition := fmt.Sprintf("%dQ%d", y, q)
+				fmt.Printf(partition)
+				fmt.Printf(" ")
+			}
+			fmt.Printf("%dQT\n", y)
+		}
+		os.Exit(0)
+	}
 
 	// 테이블이 존재하는지 점검하고 없다면 테이블을 생성한다.
 	if !validTable(*db, *tablePtr) {
